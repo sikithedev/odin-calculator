02d494757c0897f461aa32914246b254230b8e76
Muaz Sikiric <muaaz.sikiric@gmail.com>
2024-02-11 23:58:50 +0100

    Finish main part

--- script.js
+++ script.js
@@ -4,65 +4,67 @@ const operators = document.querySelectorAll(".operator");
 const equals = document.getElementById("equals");
 const clear = document.getElementById("clear");
 
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = "",
+  secondNumber = "",
+  operator = null;
 
 Array.from(digits).forEach((digit) => {
-  digit.addEventListener("click", updateDisplay);
+  digit.addEventListener("click", (e) => {
+    if (!secondNumber && e.target.textContent === "0") return;
+
+    secondNumber += e.target.textContent;
+    updateDisplay(secondNumber);
+  });
 });
 
-Array.from(operators).forEach((operator) => {
-  operator.addEventListener("click", updateDisplay);
+Array.from(operators).forEach((op) => {
+  op.addEventListener("click", (e) => {
+    if (firstNumber && operator && secondNumber) {
+      secondNumber = operate(operator, firstNumber, secondNumber);
+      updateDisplay(secondNumber);
+    }
+
+    firstNumber = secondNumber;
+    operator = e.target.textContent;
+    secondNumber = "";
+  });
 });
 
 equals.addEventListener("click", () => {
-  [firstNumber, operator, secondNumber] = display.textContent.split("");
-  display.textContent = operate(
-    operator,
-    Number(firstNumber),
-    Number(secondNumber)
-  );
+  console.log(firstNumber, operator, secondNumber);
+  if (firstNumber !== null && operator && secondNumber !== null) {
+    secondNumber = operate(operator, firstNumber, secondNumber);
+    updateDisplay(secondNumber);
+
+    if (secondNumber === "lol") secondNumber = "";
+  }
+
+  firstNumber = "";
+  operator = null;
 });
 
-clear.addEventListener("click", clearDisplay);
+clear.addEventListener("click", () => {
+  updateDisplay("0");
+  firstNumber = "";
+  secondNumber = "";
+  operator = null;
+});
 
-function updateDisplay(e) {
-  console.log(e);
-  display.textContent += e.target.textContent;
-}
-
-function clearDisplay() {
-  display.textContent = "";
-}
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  if (b === 0) return Infinity;
-
-  return a / b;
+function updateDisplay(number) {
+  display.textContent = number;
 }
 
 function operate(operator, a, b) {
   const operators = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "*": divide,
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => (b === 0 ? "lol" : a / b),
   };
 
-  if (!operators[operator] || isNaN(a) || isNaN(b)) return "ERROR";
+  console.log(a, operator, b);
+  if (a === null && b === null) return 0;
+  if (!operator || a === null) return b;
 
-  return operators[operator](a, b);
+  return operators[operator](Number(a), Number(b));
 }
